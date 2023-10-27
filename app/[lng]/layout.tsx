e30ae5eac2205a021c9c5d8054e1da3069206ab0
kjxbyz <kjxbyz@163.com>
2023-10-27 18:31:20 +0800

    fix: compatibility issues

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -28,7 +28,6 @@ export async function generateMetadata({
     title: params.lng === "en" ? "DSVideo" : "DSVideo",
     description: `${params.lng === "en" ? "DSVideo" : "DSVideo"}.`,
     metadataBase: new URL("https://kjxbyz.com"),
-    themeColor: "#FFF",
     icons: {
       icon: "/dsvideo/logo.jpg",
     },
