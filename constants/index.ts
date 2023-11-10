0ba07dd0bb5cde2d80e8e8f3265500d6085be274
kjxbyz <kjxbyz@163.com>
2023-11-10 21:41:06 +0800

    fix: update basePath

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,4 @@
 export const cacheLngKey: string = "__dsvideo_lng__";
 export const cacheThemeKey: string = "__dsvideo_theme__";
+export const basePath =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "/dsvideo" : "";
